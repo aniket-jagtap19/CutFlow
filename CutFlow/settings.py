5d407912939e488fe8a593a41ecbdd814da4e9e3
Aniket Jagtap <jagtapaniket731712@gmail.com>
2026-05-07 02:19:49 +0530

    fix allowed hosts

--- CutFlow/settings.py
+++ CutFlow/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret-key')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 # ✅ FIX: required for Render (can still work locally)
-ALLOWED_HOSTS = ['.onrender.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 INSTALLED_APPS = [
